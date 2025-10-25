50de7b570457bd8eff6eafc26e8eca482c5e576b
Andrei137 <andrei.fabian188@gmail.com>
2025-10-25 20:51:10 +0300

    [RESTRUCTURE] Create TMDB service

--- backend/external-services/metadata/tmdb/src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from '@app';
-import chalk from 'chalk';
-import config from '@FMI-MediaMaster/load-dotenv';
-
-app.listen(config.PORT, () => {
-    console.log(`${chalk.cyan('[STARTED]')} Listening at http://localhost:${config.PORT}`);
-});
-

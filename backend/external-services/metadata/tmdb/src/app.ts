0fffa291c9e7298e8622d1c6a3b6205a1b9c4d36
Giulian617 <buzatugiulian@yahoo.com>
2025-10-25 09:30:05 +0300

    [INIT] Initialize from template

---
+++ backend/external-services/metadata/tmdb/src/app.ts
@@ -0,0 +1,26 @@
+import cors from 'cors';
+import helmet from 'helmet';
+import routes from '@routes';
+import express, { Express } from 'express';
+import config from '@FMI-MediaMaster/load-dotenv';
+import {
+    unknownEndpoint,
+    responseExtensions,
+    errorHandler,
+    requestLogger
+} from '@FMI-MediaMaster/express-middleware';
+
+const app: Express = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(requestLogger(config));
+app.use(responseExtensions);
+app.use('/api', routes);
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+export default app;
+

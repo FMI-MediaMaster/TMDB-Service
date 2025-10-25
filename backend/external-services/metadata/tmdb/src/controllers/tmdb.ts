186966596c4cc1c44846ef7e84d327ca740d6334
Andrei137 <andrei.fabian188@gmail.com>
2025-10-25 18:20:43 +0300

    [FEATURE] Add movies and series endpoints

--- backend/external-services/metadata/tmdb/src/controllers/tmdb.ts
+++ backend/external-services/metadata/tmdb/src/controllers/tmdb.ts
@@ -2,8 +2,9 @@ import { Request, Response } from 'express';
 import TmdbService from '@services/tmdb';
 
 export default class TmdbController {
-    static handler(req: Request, res: Response): void {
+    static async handler(req: Request, res: Response): Promise<void> {
         const tmdb: TmdbService = new TmdbService(req.params.type!);
-        res.ok(tmdb.handle(req.params.method!, req.query));
-    }
-}
+        res.ok(await tmdb.handle(req.params.method, req.query) as object);
+    };
+};
+

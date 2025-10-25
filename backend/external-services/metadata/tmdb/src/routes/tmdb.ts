3ecac60e50247e61d1b9ee85314a6fbdfdf07c34
Andrei137 <andrei.fabian188@gmail.com>
2025-10-25 18:20:43 +0300

    [FEATURE] Add movies and series endpoints

--- backend/external-services/metadata/tmdb/src/routes/tmdb.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import tmdbController from '@controllers/tmdb';
-
-const router: Router = Router();
-
-router.get('/:type/:method', tmdbController.handler);
-
-export default router;
